0f8bd621e0204fa29ec1e129923392c7def6d563
Angelo Sciarra <angelosciarra@ymail.com>
2021-12-01 19:39:37 +0100

    little refactor

--- src/main/kotlin/org/erueka84/Day1.kt
+++ src/main/kotlin/org/erueka84/Day1.kt
@@ -4,26 +4,23 @@ import java.io.File
 
 val sonarReads = File("src/main/resources/day1.input").readLines().map { it.toInt() }
 
-fun puzzle1(): Int {
-    return sonarReads.take(sonarReads.size - 1)
+fun puzzle1(): Int =
+    sonarReads.take(sonarReads.size - 1)
         .zip(sonarReads.drop(1))
         .count { (prev, next) -> next > prev }
-}
 
 
-fun puzzle2(): Int {
-    val windowed: List<List<Int>> = sonarReads.windowed(3)
+fun puzzle2(): Int =
+    sonarReads.windowed(3).let { windowed ->
+        windowed.take(windowed.size - 1)
+            .zip(windowed.drop(1))
+            .count { (prev, next) -> next.sum() > prev.sum() }
+    }
 
-    return windowed.take(windowed.size - 1)
-        .zip(windowed.drop(1))
-        .count { (prev, next) -> next.sum() > prev.sum() }
-}
-
-fun puzzle2Bis(): Int {
-    return sonarReads.take(sonarReads.size - 3)
+fun puzzle2Bis(): Int =
+    sonarReads.take(sonarReads.size - 3)
         .zip(sonarReads.drop(3))
         .count { (prev, next) -> next > prev }
-}
 
 fun main() {
     println(puzzle1())
