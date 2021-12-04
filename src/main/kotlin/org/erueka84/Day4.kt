37861e4aa0a196f1cc4c52a1892cb9c978a3f04a
Angelo Sciarra <angelosciarra@ymail.com>
2021-12-04 16:52:43 +0100

    day4

---
+++ src/main/kotlin/org/erueka84/Day4.kt
@@ -0,0 +1,121 @@
+package org.erueka84
+
+import org.erueka84.Common.readLines
+
+object Day4 {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        println(solve())
+    }
+
+    private fun solve(): Pair<Int?, Int?> {
+        val input = readLines("/day4.input").toList()
+        val numbers = sequenceOfExtractedNumbers(input)
+        val game = initializeGame(input.drop(2))
+        numbers.forEach { n ->
+            game.draw(n)
+        }
+        return Pair(game.firstWinnerScore(), game.lastWinnerScore())
+    }
+
+    private fun initializeGame(inputs: List<String>): Game {
+        val boards = inputs.windowed(5, 6).map(Board::from)
+        return Game(boards)
+    }
+
+    private fun sequenceOfExtractedNumbers(input: List<String>) =
+        input[0].split(",").map { it.toInt() }
+
+    data class BoardNumber(val value: Int, var marked: Boolean = false) {
+        fun markIfIs(n: Int) {
+            if (value == n) marked = true
+        }
+
+        companion object {
+            fun from(rawNumber: String): BoardNumber = BoardNumber(rawNumber.toInt())
+        }
+    }
+
+    data class Board(val rows: List<List<BoardNumber>>) {
+        private lateinit var callback: (Board) -> Unit
+        private var gotBingo = false
+        private val columns: Array<Array<BoardNumber>> = Array(5) { i ->
+            Array(5) { j ->
+                rows[j][i]
+            }
+        }
+
+        fun mark(n: Int) {
+            if (!gotBingo) {
+                rows.forEach { row ->
+                    row.forEach { boardNumber ->
+                        boardNumber.markIfIs(n)
+                    }
+                }
+                checkBingo()
+            }
+        }
+
+        private fun checkBingo() {
+            if (anyRowFullyMarked() || anyColumnsFullyMarked()) {
+                gotBingo = true
+                callback(this)
+            }
+        }
+
+        private fun anyColumnsFullyMarked() = columns.any { it.all { it.marked } }
+
+        private fun anyRowFullyMarked() = rows.any { it.all { it.marked } }
+
+        fun registerBingoCallBack(callback: (Board) -> Unit) {
+            this.callback = callback
+        }
+
+        fun score(): Int {
+            return rows.flatten().filter { !it.marked }.sumOf { it.value }
+        }
+
+        companion object {
+            fun from(rawBoardRows: List<String>): Board {
+                val rows = rawBoardRows.map {
+                    val rawNumbers = it.trim().split("\\s+".toRegex())
+                    rawNumbers.map(BoardNumber::from)
+                }
+                return Board(rows)
+            }
+        }
+    }
+
+    data class Game(val boards: List<Board>) {
+
+        init {
+            boards.forEach { b -> b.registerBingoCallBack(this::bingo) }
+        }
+
+        private var _firstWinnerScore: Int? = null
+        private var _lastWinnerScore: Int? = null
+        private var drawnNumbers = ArrayDeque<Int>()
+
+        fun draw(n: Int) {
+            drawnNumbers.addFirst(n)
+            boards.forEach { board -> board.mark(n) }
+        }
+
+        private fun bingo(board: Board) {
+            val score = board.score() * drawnNumbers.first()
+            if (_firstWinnerScore == null) {
+                _firstWinnerScore = score
+            }
+            _lastWinnerScore = score
+        }
+
+        fun firstWinnerScore(): Int? {
+            return _firstWinnerScore
+        }
+
+        fun lastWinnerScore(): Int? {
+            return _lastWinnerScore
+        }
+    }
+}
