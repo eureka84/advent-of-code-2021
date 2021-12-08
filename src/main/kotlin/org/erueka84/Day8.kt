275a500c18fbcb44ffb1bc8541f5f640b11b1b34
Angelo Sciarra <angelosciarra@ymail.com>
2021-12-08 22:56:00 +0100

    day8 part 2

--- src/main/kotlin/org/erueka84/Day8.kt
+++ src/main/kotlin/org/erueka84/Day8.kt
@@ -4,28 +4,14 @@ import org.erueka84.Common.readLines
 
 object Day8 {
 
-    private val DIGITS_WITH_UNIQUE_LENGTH_REPRESENTATION = listOf(1, 4, 7, 8)
-
-    private val normalDigitsConf = mapOf(
-        0 to "abcefg",
-        1 to "cf",
-        2 to "acdeg",
-        3 to "acdfg",
-        4 to "bcdf",
-        5 to "abdfg",
-        6 to "abdefg",
-        7 to "acf",
-        8 to "abcdefg",
-        9 to "abcdfg"
-    )
-
     @JvmStatic
     fun main(args: Array<String>) {
-        println(part1()) // 548
+        val input = readLines("/day8.input")
+        println(part1(input)) // 548
+        println(part2(input))
     }
 
-    private fun part1(): Int {
-        val input = readLines("/day8.input")
+    private fun part1(input: Sequence<String>): Int {
         val readingDigitsByRepresentationSize = readingDigits(input).groupingBy { it.length }.eachCount()
 
         return DIGITS_WITH_UNIQUE_LENGTH_REPRESENTATION
@@ -38,4 +24,89 @@ object Day8 {
         val readingDigits = reading.trim().split(" ")
         readingDigits
     }
+
+    private fun part2(input: Sequence<String>): Int =
+        input
+            .map { line -> Display.from(line) }
+            .sumOf { display -> display.reading() }
+
+    data class Display(
+        private val scrambledReading: List<String>,
+        private val scrambledUniqueDigits: List<String>
+    ) {
+        fun reading(): Int {
+            return scrambledReading.map { digitsMap[it]!! }.joinToString(separator = "").toInt()
+        }
+
+        private val digitsMap: Map<String, Char> by lazy {
+            val byLength: Map<Int, List<String>> = scrambledUniqueDigits.groupBy { it.length }
+            val result = mutableMapOf<String, Char>()
+
+            val one = byLength[2]!!.first()
+            val four = byLength[4]!!.first()
+            val seven = byLength[3]!!.first()
+            val eight = byLength[7]!!.first()
+            result[one] = '1'
+            result[four] = '4'
+            result[seven] = '7'
+            result[eight] = '8'
+
+            val oneOfTwoThreeOrFive = byLength[5]!!
+            val three = oneOfTwoThreeOrFive.find { it.containsAllCharsOf(one) }!!
+            result[three] = '3'
+
+            val twoOrFive = oneOfTwoThreeOrFive - three
+            val five = twoOrFive.find { it.containsAllCharsOf(four - one) }!!
+            result[five] = '5'
+
+            val two = (twoOrFive - five)[0]
+            result[two] = '2'
+
+            val oneOfZeroSixOrNine = byLength[6]!!
+            val nine = oneOfZeroSixOrNine.find { it.containsAllCharsOf(four) }!!
+            result[nine] = '9'
+
+            val zeroOrSix = oneOfZeroSixOrNine - nine
+            val zero = zeroOrSix.find { it.containsAllCharsOf(one) }!!
+            result[zero] = '0'
+
+            val six = (zeroOrSix - zero)[0]
+            result[six] = '6'
+
+            result
+        }
+
+        companion object {
+            fun from(line: String): Display {
+                val (rawUniqueDigits, rawReading) = line.split("|")
+                val scrambledReading = rawReading.trim().split(" ").map { sortChars(it) }
+                val uniqueDigits = rawUniqueDigits.trim().split(" ").map { sortChars(it) }
+                return Display(scrambledReading, uniqueDigits)
+            }
+
+            private fun sortChars(it: String) = it.toCharArray().sorted().joinToString(separator = "")
+        }
+
+    }
+
+    private val DIGITS_WITH_UNIQUE_LENGTH_REPRESENTATION = listOf(1, 4, 7, 8)
+
+    private val normalDigitsConf = mapOf(
+        1 to "cf",     //     C     F   - 2
+        7 to "acf",    // A   C     F   - 3
+        4 to "bcdf",   //   B C D   F   - 4
+        2 to "acdeg",  // A   C D E   G - 5
+        3 to "acdfg",  // A   C D   F G - 5
+        5 to "abdfg",  // A B   D   F G - 5
+        0 to "abcefg", // A B C   E F G - 6
+        6 to "abdefg", // A B   D E F G - 6
+        9 to "abcdfg", // A B C D   F G - 6
+        8 to "abcdefg" // A B C D E F G - 7
+    )
+
+    private operator fun String.minus(s: String): String {
+        return s.toCharArray().fold(this) { acc: String, curr: Char -> acc.replace(curr.toString(), "") }
+    }
+
+    private fun String.containsAllCharsOf(other: String): Boolean = other.all { this.contains(it) }
 }
---
+++ src/test/kotlin/org/erueka84/Day8Test.kt
@@ -0,0 +1,19 @@
+package org.erueka84
+
+import org.erueka84.Day8.Display
+import org.hamcrest.CoreMatchers.equalTo
+import org.hamcrest.MatcherAssert.assertThat
+import org.junit.Test
+
+class Day8Test{
+
+    @Test
+    fun `parse display`() {
+
+        val display =
+            Display.from("be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe")
+
+        assertThat(display.reading(), equalTo(8394))
+
+    }
+}
