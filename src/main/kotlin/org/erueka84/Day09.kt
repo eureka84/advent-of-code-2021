3f7619aad065e2317f70d55391ec07d150687734
Angelo Sciarra <angelosciarra@ymail.com>
2021-12-09 23:54:29 +0100

    day 9 part 2

--- src/main/kotlin/org/erueka84/Day09.kt
+++ src/main/kotlin/org/erueka84/Day09.kt
@@ -1,6 +1,7 @@
 package org.erueka84
 
 import org.erueka84.Common.readLines
+import java.util.*
 
 object Day09 {
 
@@ -8,9 +9,11 @@ object Day09 {
     fun main(args: Array<String>) {
         val input = readLines("/day09.input")
         println(part1(input)) // 550
+        println(part2(input)) // 1100682
     }
 
     private fun part1(input: Sequence<String>): Int = Grid.from(input).riskLevel()
+    private fun part2(input: Sequence<String>): Int = Grid.from(input).basinsAreas()
 
     data class Grid(private val map: List<List<Point>>) {
 
@@ -30,23 +33,70 @@ object Day09 {
         fun riskLevel(): Int = map.flatten().filter { it.isLow }.sumOf { it.height +1 }
 
         private fun pointIsLowerThanAllHisNeighbours(i: Int, j: Int) =
-            neighbours(i, j).all { map[i][j].height < it.height }
+            map[i][j].listOfAdjacentOn(map).all { map[i][j].height < it.height }
 
-        private fun neighbours(i: Int, j: Int): List<Point> {
-            val list = mutableListOf<Point>()
-            if (i - 1 >= 0) list.add(map[i - 1][j])
-            if (i + 1 < rows) list.add(map[i + 1][j])
-            if (j - 1 >= 0) list.add(map[i][j - 1])
-            if (j + 1 < cols) list.add(map[i][j + 1])
-            return list
+        fun basinsAreas(): Int {
+            val visited = mutableMapOf<Point, Boolean>()
+            val basinsAreas = mutableListOf<Int>()
+
+            map.indices.forEach { i ->
+                map[i].indices.forEach { j ->
+                    val node = map[i][j]
+                    if (!visited.containsKey(node) && map.isNotOnTopOfAHill(node)) {
+                        val basin = findBasin(map, visited, node)
+                        basinsAreas.add(basin.size)
+                    }
+                }
+            }
+
+            basinsAreas.sortDescending()
+
+            return basinsAreas.take(3).reduce { acc, curr -> acc * curr}
+        }
+
+        private fun findBasin(map: List<List<Point>>, visited: MutableMap<Point, Boolean>, node: Point): List<Point> {
+            visited[node] = true
+            val deque: Queue<Point> = LinkedList()
+            deque.add(node)
+            val basin = mutableListOf<Point>()
+            basin.add(node)
+            while (deque.isNotEmpty()) {
+                deque.poll().listOfAdjacentOn(map).forEach { adj ->
+                    if (map.isNotOnTopOfAHill(adj) && !visited.containsKey(adj)) {
+                        visited[adj] = true
+                        deque.add(adj)
+                        basin.add(adj)
+                    }
+                }
+            }
+            return basin
         }
 
         companion object {
             fun from(input: Sequence<String>): Grid =
-                input.toList().map { it.map { c -> Point(c.digitToInt()) } }.let { Grid(it) }
+                input.toList().mapIndexed { i, line ->
+                    line.mapIndexed { j, c ->
+                        Point(i, j, c.digitToInt())
+                    }
+                }.let { Grid(it) }
         }
     }
 
-    data class Point(val height: Int, var isLow: Boolean = false)
+    data class Point(val x: Int, val y: Int, val height: Int, var isLow: Boolean = false) {
+        private val contiguousPositions = listOf(Pair(-1, 0), Pair(0, 1), Pair(0, -1), Pair(1, 0))
+
+        fun listOfAdjacentOn(map: List<List<Point>>) =
+            contiguousPositions
+                .map { (x, y) -> Pair(this.x + x, this.y + y) }
+                .filter { (i, j) -> map.exists(i, j) }
+                .map { (i, j) -> map[i][j] }
+    }
+
+    fun List<List<Point>>.exists(i: Int, j: Int): Boolean =
+        (this.size > i && i >= 0) && (this[i].size > j && j >= 0)
+
+    fun List<List<Point>>.isNotOnTopOfAHill(node: Point): Boolean =
+        this.exists(node.x, node.y) && this[node.x][node.y].height != 9
 
 }
+
