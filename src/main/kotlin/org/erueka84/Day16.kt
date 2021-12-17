8958a7c66bc084f23246e50116aab1b6b0b7a4fd
Angelo Sciarra <angelosciarra@ymail.com>
2021-12-17 18:53:49 +0100

    day 16 part 2

--- src/main/kotlin/org/erueka84/Day16.kt
+++ src/main/kotlin/org/erueka84/Day16.kt
@@ -8,6 +8,11 @@ object Day16 {
     fun main(args: Array<String>) {
         val packet = readPacket()
         println(part1(packet)) // 949
+        println(part2(packet)) //
+    }
+
+    private fun part2(packet: Packet): Long {
+        return packet.evaluate()
     }
 
     private fun part1(packet: Packet): Int {
@@ -67,6 +72,8 @@ object Day16 {
         abstract val version: Int
         abstract val type: Int
         abstract val length: Int
+
+        abstract fun evaluate(): Long
     }
 
     data class Literal(override val version: Int, val payload: List<String>) : Packet() {
@@ -76,6 +83,10 @@ object Day16 {
             get() = 4
         override val length: Int
             get() = HEADERS_LENGTH + 5 * payload.size
+
+        override fun evaluate(): Long {
+            return payload.joinToString(separator = "").toLong(2)
+        }
     }
 
     data class Operator(override val version: Int, override val type: Int, val packets: List<Packet>) : Packet() {
@@ -83,6 +94,18 @@ object Day16 {
             get() = version + packets.sumOf { it.versionSums }
         override val length: Int
             get() = HEADERS_LENGTH + packets.sumOf { it.length }
+
+        override fun evaluate(): Long {
+            return when(type) {
+                0 -> packets.sumOf { it.evaluate() }
+                1 -> packets.fold(1) {acc, packet -> acc * packet.evaluate() }
+                2 -> packets.minOf { it.evaluate() }
+                3 -> packets.maxOf { it.evaluate() }
+                5 -> if (packets[0].evaluate() > packets[1].evaluate()) 1 else 0
+                6 -> if (packets[0].evaluate() < packets[1].evaluate()) 1 else 0
+                else -> if (packets[0].evaluate() == packets[1].evaluate()) 1 else 0
+            }
+        }
     }
 
     fun CharIterator.next(n: Int): String = (1..n).map { next() }.toCharArray().let { String(it) }
