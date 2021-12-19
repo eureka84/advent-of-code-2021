eda112e72a495d8ad60541aea5e8d9659d1a7b5d
Angelo Sciarra <angelosciarra@ymail.com>
2021-12-19 20:53:09 +0100

    day 17 part 1

--- src/main/kotlin/org/erueka84/Day17.kt
+++ src/main/kotlin/org/erueka84/Day17.kt
@@ -1,12 +1,31 @@
 package org.erueka84
 
 import org.erueka84.Common.readLines
+import org.erueka84.MathLib.triangular
 
 object Day17 {
 
     @JvmStatic
     fun main(args: Array<String>) {
-        readLines("/day17.input")
+        val input = readLines("/day17.input").first()
+        println(part1(input))
+    }
+
+    private fun part1(input: String): Int {
+        val targetArea = TargetArea.parse(input)
+        return targetArea.y0.triangular()
+    }
+
+    data class TargetArea(val x0: Int, val x1:Int, val y0: Int, val y1:Int) {
+        companion object {
+            fun parse(input: String): TargetArea {
+                val number = "(-?\\d+)"
+                val twoDots = "\\.{2}"
+                val regex = "target area: x=$number$twoDots$number, y=$number$twoDots$number".toRegex()
+                val (_, x0, x1, y0, y1) = regex.matchEntire(input)!!.groupValues
+                return TargetArea(x0.toInt(), x1.toInt(), y0.toInt(), y1.toInt())
+            }
+        }
     }
 
 }
